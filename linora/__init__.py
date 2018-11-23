c164fbdd6cf58afb23b89a664217891e3552fe33
JinQing Lee <hourout@163.com>
2018-11-23 21:26:27 +0800

    Update __init__.py

--- linora/__init__.py
+++ linora/__init__.py
@@ -1,14 +1,10 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+
 
 from linora import XGBClassifier
 from linora import XGBRanker
 from linora import XGBRegressor
 
-__version__ = '0.6.0'
+__version__ = '0.1.0'
 __author__ = 'JinQing Lee, Gaojie Wei'
 
-del absolute_import
-del division
-del print_function
+__all__ = ['XGBClassifier', 'XGBRanker', 'XGBRegressor'
