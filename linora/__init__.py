810d51d97c4ed13c56094ac54760eae7e8772a22
JinQing Lee <hourout@163.com>
2018-11-23 20:59:08 +0800

    Update __init__.py

--- linora/__init__.py
+++ linora/__init__.py
@@ -1,3 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from linora import XGBClassifier
 from linora import XGBRanker
 from linora import XGBRegressor
+
+__version__ = '0.6.0'
+__author__ = 'JinQing Lee, Gaojie Wei'
+
+del absolute_import
+del division
+del print_function
