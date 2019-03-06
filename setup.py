d6a53b0487b87bfe69ec52b920e862380f6ae45d
JinQing Lee <hourout@163.com>
2019-03-06 18:23:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ def readme():
     with open('README.md', encoding='utf-8') as f:
         return f.read()
 
+requires_list=['pandas>=0.24.1', 'scikit-learn>=0.20.2', 'xgboost>=0.81', 'pyecharts>=0.5.11',
+              'pyecharts_snapshot>=0.1.10', 'numpy>=1.16.2']
+
 setup(name='linora',
-      version='0.3.0',
-      install_requires=['pandas>=0.20.3', 'scikit-learn>=0.19.1', 'xgboost>=0.81'],
+      version='0.4.0',
+      install_requires=requires_list,
       description='Easy automatic hyperparameter optimization algorithms and libraries for XGBoost and LightGBM.',
       long_description=readme(),
       long_description_content_type='text/markdown',
@@ -25,7 +28,8 @@ setup(name='linora',
           'Topic :: Scientific/Engineering :: Visualization',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7'
       ],
       packages=find_packages(),
       zip_safe=False)
