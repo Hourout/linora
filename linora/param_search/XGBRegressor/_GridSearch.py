a2308574324ab3a8b3f6dac2422c0dd2dbd85032
JinQing Lee <hourout@163.com>
2019-03-06 11:13:35 +0800

    Update _GridSearch.py

--- linora/param_search/XGBRegressor/_GridSearch.py
+++ linora/param_search/XGBRegressor/_GridSearch.py
@@ -10,6 +10,26 @@ from sklearn.model_selection import KFold, train_test_split
 
 def GridSearch(feature, label, loss, metrics, scoring=0.5, cv=5, cv_num=3,
                metrics_min=True, speedy=True, speedy_param=(20000, 0.3), gpu=False):
+    """XGBRegressor model params search use GridSearch method.
+    
+    Args:
+        feature: pandas dataframe, model's feature.
+        label: pandas series, model's label.
+        loss: XGBClassifier param 'objective'.
+        metrics: model metrics function.
+        scoring: metrics error opt base line value.
+        cv: cross validation fold.
+        cv_num: minimum cross validation fold.
+        metrics_min: metrics value whether the smaller the better.
+        speedy: whether use speedy method.
+        speedy_param: if use speedy method, test_size will be set, 
+                      test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2).
+        gpu: whether use gpu.
+    Returns:
+        a best XGBRegressor model params dict.
+    Raises:
+        params error.
+    """
     def product(x):
         if len(x)==1:
             return itertools.product(x[0])
@@ -19,7 +39,7 @@ def GridSearch(feature, label, loss, metrics, scoring=0.5, cv=5, cv_num=3,
             return itertools.product(x[0], x[1], x[2])
     start = time.time()
     if speedy:
-        train_size = round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2)
+        test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2)
     tree_method = 'gpu_hist' if gpu else 'auto'
     n_job = 1 if gpu else int(np.ceil(cpu_count()*0.8))
     params = {'learning_rate': 0.1, 'n_estimators': 300, 'max_depth': 5, 'min_child_weight': 1,
@@ -47,7 +67,7 @@ def GridSearch(feature, label, loss, metrics, scoring=0.5, cv=5, cv_num=3,
             score = []
             if speedy:
                 for i in range(cv_num):
-                    X_train, X_test, y_train, y_test = train_test_split(feature, label, train_size=train_size,
+                    X_train, X_test, y_train, y_test = train_test_split(feature, label, test_size=test_size,
                                                                         random_state=np.random.choice(range(100), 1)[0])
                     model.fit(X_train, y_train)
                     cv_pred = model.predict(X_test)
