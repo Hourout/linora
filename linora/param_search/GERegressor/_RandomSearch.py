94aee92eb23606cb293d5000cf0dba38f3a8905e
JinQing Lee <hourout@163.com>
2019-03-06 11:23:15 +0800

    Update _RandomSearch.py

--- linora/param_search/GERegressor/_RandomSearch.py
+++ linora/param_search/GERegressor/_RandomSearch.py
@@ -8,17 +8,38 @@ from sklearn.model_selection import KFold, train_test_split
 
 def RandomSearch(feature, label, estimator, params, metrics, iter_num=1000, scoring=0.5, cv=5, cv_num=3,
                  metrics_min=True, speedy=True, speedy_param=(20000, 0.3)):
+    """General model params search use RandomSearch method.
+    
+    Args:
+        feature: pandas dataframe, model's feature.
+        label: pandas series, model's label.
+        estimator: general model.
+        params: a general model params dict.
+        metrics: model metrics function.
+        scoring: metrics error opt base line value.
+        cv: cross validation fold.
+        cv_num: minimum cross validation fold.
+        metrics_min: metrics value whether the smaller the better.
+        speedy: whether use speedy method.
+        speedy_param: if use speedy method, test_size will be set, 
+                      test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2).
+        gpu: whether use gpu.
+    Returns:
+        a best General model params dict.
+    Raises:
+        params error.
+    """
     start = time.time()
     best_params={}
     if speedy:
-        train_size = round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2)
+        test_size = round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2)
     for i in range(1, iter_num+1):
         param = {i:np.random.choice(j) for i,j in params.items()}
         model = estimator(**param)
         score = []
         if speedy:
             for _ in range(cv_num):
-                X_train, X_test, y_train, y_test = train_test_split(feature, label, train_size=train_size,
+                X_train, X_test, y_train, y_test = train_test_split(feature, label, test_size=test_size,
                                                                     random_state=np.random.choice(range(100), 1)[0])
                 model.fit(X_train, y_train)
                 cv_pred = model.predict(X_test)
