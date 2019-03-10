26490285980f71368bdb1b2a1b90cae1d94ae95b
JinQing Lee <hourout@163.com>
2019-03-10 14:34:07 +0800

    Update _normalize.py

--- linora/feature_column/_normalize.py
+++ linora/feature_column/_normalize.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-__all__ ['normalize_minmax', 'normalize_maxabs', 'normalize_max', 'normalize_l1', 'normalize_l2']
+__all__ ['normalize_minmax', 'normalize_maxabs', 'normalize_max', 'normalize_l1', 'normalize_l2',
+         'normalize_norm']
 
 def normalize_minmax(feature, feature_range=(0, 1), feature_scale=None):
     scale = feature_scale if feature_scale is not None else (feature.min(), feature.max())
@@ -25,4 +26,9 @@ def normalize_l1(feature, feature_scale=None):
 def normalize_l2(feature, feature_scale=None):
     scale = feature_scale if feature_scale is not None else np.sqrt(np.sum(np.square(feature)))
     t = feature/scale
+    return t, scale
+
+def normalize_norm(feature, feature_scale=(0, 1)):
+    scale = (0, 1) if feature_scale==(0, 1) else (feature.mean(), feature.std())
+    t = (feature-scale[0])/scale[1]
     return t, scale
