08563c4107f3ea8009a96615279532ecf89504e1
JinQing Lee <hourout@163.com>
2019-03-14 11:14:54 +0800

    Update _divergence.py

--- linora/metrics/distance/_divergence.py
+++ linora/metrics/distance/_divergence.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-__all__ = ['kl_divergence', 'js_divergence']
+__all__ = ['kl_divergence', 'js_divergence', 'mutual_information_rate', 'pointwise_mutual_information_rate']
 
 def kl_divergence(x, y, continuous=False, bucket_num=1000):
     if continuous:
@@ -30,3 +30,38 @@ def js_divergence(x, y, continuous=False, bucket_num=1000):
     t1 = np.sum(np.log((t.prob_x+0.00001)/(t.prob_m+0.00001))*t.prob_x)
     t2 = np.sum(np.log((t.prob_y+0.00001)/(t.prob_m+0.00001))*t.prob_y)
     return 0.5*(t1+t2)
+
+def mutual_information(feature1, feature2):
+    t = feature1.value_counts(normalize=True).reset_index()
+    t.columns = ['label', 'label_mean']
+    t['temp'] = 0
+    t1 = feature2.value_counts(normalize=True).reset_index()
+    t1.columns = ['label1', 'label1_mean']
+    t1['temp'] = 0
+    t = t.merge(t1, on='temp')
+    t['label2'] = t.label.astype(str)+'-'+t.label1.astype(str)
+    t2 = (feature1.astype(str)+'-'+feature2.astype(str)).value_counts(normalize=True).reset_index()
+    t2.columns = ['label2', 'label2_mean']
+    t = t.merge(t2, on='label2', how='left').dropna()
+    t['mutual_information'] = t.label2_mean*np.log2(t.label2_mean/t.label_mean/t.label1_mean)
+    return t.mutual_information.sum()
+
+def pointwise_mutual_information(feature1, feature2):
+    t = feature1.value_counts(normalize=True).reset_index()
+    t.columns = ['label', 'label_mean']
+    t['temp'] = 0
+    t1 = feature2.value_counts(normalize=True).reset_index()
+    t1.columns = ['label1', 'label1_mean']
+    t1['temp'] = 0
+    t = t.merge(t1, on='temp')
+    t['label2'] = t.label.astype(str)+'-'+t.label1.astype(str)
+    t2 = (feature1.astype(str)+'-'+feature2.astype(str)).value_counts(normalize=True).reset_index()
+    t2.columns = ['label2', 'label2_mean']
+    t = t.merge(t2, on='label2', how='left').dropna()
+    return np.power(np.cumprod(np.log2(t.label2_mean/t.label_mean/t.label1_mean)).max(), 1/len(t))
+
+def mutual_information_rate(feature1, feature2):
+    return mutual_information(feature1, feature2)/min(mutual_information(feature1, feature1), mutual_information(feature2, feature2))
+
+def pointwise_mutual_information_rate(feature1, feature2):
+    return pointwise_mutual_information(feature1, feature2)/min(pointwise_mutual_information(feature1, feature1), pointwise_mutual_information(feature2, feature2))
