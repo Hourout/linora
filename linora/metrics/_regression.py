546f730436ed976905d1e553c869c5de9de931bc
JinQing Lee <hourout@163.com>
2019-03-19 15:34:37 +0800

    Update _regression.py

--- linora/metrics/_regression.py
+++ linora/metrics/_regression.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-__all__ = ['normal_loss', 'mean_absolute_error', 'mean_squared_error']
+__all__ = ['normal_loss', 'mean_absolute_error', 'mean_squared_error',
+           'mean_absolute_percentage_error', 'hinge']
 
 def normal_loss(y_true, y_pred, k, log=False, root=False):
     if log:
@@ -17,3 +18,9 @@ def mean_absolute_error(y_true, y_pred, log=False, root=False):
 
 def mean_squared_error(y_true, y_pred, log=False, root=False):
     return normal_loss(y_true, y_pred, k=2, log=log, root=root)
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    return ((y_true-y_pred)/y_true).abs().mean()
+
+def hinge(y_true, y_pred, k=1):
+    return (1-y_true*y_pred).clip(lower=0).pow(k).mean()
