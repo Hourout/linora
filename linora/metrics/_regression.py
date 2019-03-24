4241435611cada42dcc4d5cc68ee6928e1f2e9e5
JinQing Lee <hourout@163.com>
2019-03-24 16:26:54 +0800

    Update _regression.py

--- linora/metrics/_regression.py
+++ linora/metrics/_regression.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 __all__ = ['normal_loss', 'mean_absolute_error', 'mean_squared_error',
-           'mean_absolute_percentage_error', 'hinge']
+           'mean_absolute_percentage_error', 'hinge', 'explained_variance_score',
+           'median_absolute_error', 'r2_score']
 
 def normal_loss(y_true, y_pred, k, log=False, root=False):
     if log:
@@ -24,3 +25,12 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 def hinge(y_true, y_pred, k=1):
     return (1-y_true*y_pred).clip(lower=0).pow(k).mean()
+
+def explained_variance_score(y_true, y_pred):
+    return 1-(y_true-y_pred).std()**2/y_true.std()**2
+
+def median_absolute_error(y_true, y_pred):
+    return (y_true-y_pred).abs().median()
+
+def r2_score(y_true, y_pred):
+    return 1-(y_true-y_pred).pow(2).sum()/(y_true-y_true.mean()).pow(2).sum()
