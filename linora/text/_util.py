b1275ddd7649eaa3cf6cbca4044b0a3498be6775
JinQing Lee <hourout@163.com>
2019-04-03 18:40:56 +0800

    Create _util.py

---
+++ linora/text/_util.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+__all__ = ['select_best_length']
+
+def select_best_length(sequence, sample_rate=0.8):
+    t = sorted(map(lambda x:len(x), sequence))
+    return t[int(np.ceil(len(sample_rate)*0.7))]
