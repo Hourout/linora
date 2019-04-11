1f4970c297735bd6a8cfc76064b623369a181039
JinQing Lee <hourout@163.com>
2019-04-11 18:26:06 +0800

    Update _credit.py

--- linora/feature_selection/_credit.py
+++ linora/feature_selection/_credit.py
@@ -4,6 +4,21 @@ import pandas as pd
 __all__ = ['woe', 'iv']
 
 def woe(y_true, feature, pos_label=1):
+    """Calculate series woe value
+    
+    Parameters
+    ----------
+    y_true   : pd.Series, shape (n_samples,)
+        The target variable for supervised learning problems.
+    feature  : pd.Series, shape (n_samples,)
+        x variable, model feature.
+    pos_label : int, default=1
+        positive label value.
+    
+    Returns
+    -------
+    t : series woe value
+    """
     t = pd.DataFrame({'label':y_true, 'feature':feature})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
     label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
@@ -13,6 +28,21 @@ def woe(y_true, feature, pos_label=1):
     return t
 
 def iv(y_true, feature, pos_label=1):
+    """Calculate series iv value
+    
+    Parameters
+    ----------
+    y_true   : pd.Series, shape (n_samples,)
+        The target variable for supervised learning problems.
+    feature  : pd.Series, shape (n_samples,)
+        x variable, model feature.
+    pos_label : int, default=1
+        positive label value.
+    
+    Returns
+    -------
+    t : series iv value
+    """
     t = pd.DataFrame({'label':y_true, 'feature':feature})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
     label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
