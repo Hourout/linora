d7c977437059d48bea0552c0daa423aeda529941
JinQing Lee <hourout@163.com>
2019-05-08 14:08:11 +0800

    Update __init__.py

--- linora/__init__.py
+++ linora/__init__.py
@@ -7,14 +7,14 @@ from linora import sample_splits
 from linora import text
 
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'JinQing Lee'
 def _hello():
     print("""
 ------------------------------------------------------------------------------------
       Linora
 --------------------
-      Version      : -- 0.5.0  --
+      Version      : -- 0.6.0  --
       Author       : JinQing Lee
       License      : Apache-2.0
       Homepage     : https://github.com/Hourout/linora
