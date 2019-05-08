9ae964a2ae2a4640478502a1129e95ffe527aae7
JinQing Lee <hourout@163.com>
2019-05-08 14:04:51 +0800

    update to pyecharts1.0.0

--- linora/chart/_metrics.py
+++ linora/chart/_metrics.py
@@ -18,14 +18,15 @@ def ks_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Kolmogorov-Smirnov
     t['fpr'] = t.fp/(t.label.count()-t.label.sum())
     t['ks'] = (t.tpr-t.fpr).abs().round(4)
     t.index = np.round(((t.index+1)/len(t)), 2)
-    line = pe.Line("Kolmogorov-Smirnov Curve")
-    line.add("TPR", [0]+t.index.tolist(), [0]+t.tpr.round(4).tolist(),
-             is_smooth=True, yaxis_type='value', xaxis_type='value')
-    line.add("FPR", [0]+t.index.tolist(), [0]+t.fpr.round(4).tolist(),
-             is_smooth=True, yaxis_type='value', xaxis_type='value')
-    line.add("KS", [0]+t.index.tolist(), [0]+t.ks.tolist(),
-             is_smooth=True, mark_point=["max"], yaxis_type='value', xaxis_type='value')
-    return line if jupyter else line.render(path)
+    line = (pe.charts.Line()
+        .add_xaxis([0]+t.index.tolist())
+        .add_yaxis("TPR", [0]+t.tpr.round(4).tolist(), is_smooth=True)
+        .add_yaxis("FPR", [0]+t.fpr.round(4).tolist(), is_smooth=True)
+        .add_yaxis("KS", [0]+t.ks.tolist(), is_smooth=True, markpoint_opts=pe.options.MarkPointOpts(data=[pe.options.MarkPointItem(type_='max', name='最大值')]))
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="Kolmogorov-Smirnov Curve"),
+                         xaxis_opts=pe.options.AxisOpts(is_scale=True, type_='value')))
+    return line.render_notebook() if jupyter else line.render(path)
 
 def roc_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Receiver Operating Characteristic Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
@@ -37,14 +38,17 @@ def roc_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Receiver Operatin
     t['fp'] = t.index+1-t.tp
     t['tpr'] = t.tp/t.label.sum()
     t['fpr'] = t.fp/(t.label.count()-t.label.sum())
-    line = pe.Line("ROC Curve")
-    line.add("ROC", [0]+t.fpr.round(4).tolist(), [0]+t.tpr.round(4).tolist(),
-             is_smooth=True, is_fill=True, area_opacity=0.4,
-             yaxis_type='value', xaxis_type='value')
-    line.add("Random", [0]+t.fpr.round(4).tolist(), [0]+t.fpr.round(4).tolist(),
-             yaxis_type='value', xaxis_type='value', xaxis_name='FPR', yaxis_name='TPR')
-    return line if jupyter else line.render(path)
-
+    line = (pe.charts.Line()
+        .add_xaxis([0]+t.fpr.round(4).tolist())
+        .add_yaxis("ROC", [0]+t.tpr.round(4).tolist(), is_smooth=True)
+        .add_yaxis("Random", [0]+t.fpr.round(4).tolist())
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False),
+                         areastyle_opts=pe.options.AreaStyleOpts(0.4))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="ROC Curve"),
+                         xaxis_opts=pe.options.AxisOpts('FPR', is_scale=True, type_='value'),
+                         yaxis_opts=pe.options.AxisOpts('TPR')))
+    return line.render_notebook() if jupyter else line.render(path)
+    
 def pr_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Precision Recall Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
@@ -55,11 +59,15 @@ def pr_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Precision Recall C
     t['fp'] = t.index+1-t.tp
     t['recall'] = t.tp/t.label.sum()
     t['precision'] = t.tp/(t.tp+t.fp)
-    line = pe.Line("PR Curve")
-    line.add("PR", [0]+t.recall.round(4).tolist(), [1]+t.precision.round(4).tolist(),
-             is_smooth=True, is_fill=True, area_opacity=0.4,
-             yaxis_type='value', xaxis_type='value', xaxis_name='recall', yaxis_name='precision')
-    return line if jupyter else line.render(path)
+    line = (pe.charts.Line()
+        .add_xaxis([0]+t.recall.round(4).tolist())
+        .add_yaxis("PR", [1]+t.precision.round(4).tolist(), is_smooth=True)
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False),
+                         areastyle_opts=pe.options.AreaStyleOpts(0.4))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="PR Curve"),
+                         xaxis_opts=pe.options.AxisOpts('recall', is_scale=True, type_='value'),
+                         yaxis_opts=pe.options.AxisOpts('precision')))
+    return line.render_notebook() if jupyter else line.render(path)
 
 def lift_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Lift Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
@@ -72,12 +80,14 @@ def lift_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Lift Curve.html'
     t['precision'] = t.tp/(t.tp+t.fp)
     t['lift'] = t.precision/t.label.sum()*t.label.count()
     t.index = np.round(((t.index+1)/len(t)), 2)
-    line = pe.Line("Lift Curve")
-    line.add("Lift", t.index.tolist(), t.lift.round(3).tolist(), is_smooth=True,
-             yaxis_type='value', xaxis_type='value')
-    line.add("Random", t.index.tolist(), [1]*len(t), is_smooth=True,
-             yaxis_type='value', xaxis_type='value')
-    return line if jupyter else line.render(path)
+    line = (pe.charts.Line()
+        .add_xaxis(t.index.tolist())
+        .add_yaxis("Lift", t.lift.round(3).tolist(), is_smooth=True)
+        .add_yaxis("Random", [1]*len(t), is_smooth=True)
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="Lift Curve"),
+                         xaxis_opts=pe.options.AxisOpts(is_scale=True, type_='value')))
+    return line.render_notebook() if jupyter else line.render(path)
 
 def gain_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gain Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
@@ -89,10 +99,13 @@ def gain_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gain Curve.html'
     t['fp'] = t.index+1-t.tp
     t['precision'] = t.tp/(t.tp+t.fp)
     t.index = np.round(((t.index+1)/len(t)), 2)
-    line = pe.Line("Gain Curve")
-    line.add("Gain", t.index.tolist(), t.precision.round(4).tolist(),
-             is_smooth=True, yaxis_type='value', xaxis_type='value')
-    return line if jupyter else line.render(path)
+    line = (pe.charts.Line()
+        .add_xaxis(t.index.tolist())
+        .add_yaxis("Gain", t.precision.round(4).tolist(), is_smooth=True)
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="Gain Curve"),
+                         xaxis_opts=pe.options.AxisOpts(is_scale=True, type_='value')))
+    return line.render_notebook() if jupyter else line.render(path)
 
 def gini_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gini Curve.html'):
     t = pd.DataFrame({'prob':y_pred, 'label':y_true})
@@ -102,21 +115,25 @@ def gini_curve(y_true, y_pred, pos_label=1, jupyter=True, path='Gini Curve.html'
     t = t.sort_values(['prob', 'label']).reset_index(drop=True)
     t['cum'] = t.label.cumsum()/t.label.sum()
     t.index = np.round(((t.index+1)/len(t)), 2)
-    line = pe.Line("Gini Curve")
-    line.add("Random", [0]+t.index.tolist(), [0]+t.index.tolist(),
-             is_fill=True, area_opacity=0.4,
-             is_smooth=True, yaxis_type='value', xaxis_type='value')
-    line.add("Gini", [0]+t.index.tolist(), [0]+t.cum.round(4).tolist(),
-             is_fill=True, area_opacity=0.4,
-             is_smooth=True, yaxis_type='value', xaxis_type='value')
-    return line if jupyter else line.render(path)
+    line = (pe.charts.Line()
+        .add_xaxis([0]+t.index.tolist())
+        .add_yaxis("Gini", [0]+t.cum.round(4).tolist(), is_smooth=True)
+        .add_yaxis("Random", [0]+t.index.tolist())
+        .set_series_opts(label_opts=pe.options.LabelOpts(is_show=False),
+                         areastyle_opts=pe.options.AreaStyleOpts(0.4))
+        .set_global_opts(title_opts=pe.options.TitleOpts(title="Gini Curve"),
+                         xaxis_opts=pe.options.AxisOpts(is_scale=True, type_='value')))
+    return line.render_notebook() if jupyter else line.render(path)
 
 def confusion_matrix_map(y_true, y_pred, jupyter=True, path="Confusion Matrix Map.html"):
     t = confusion_matrix(y_true, y_pred)
     t = pd.DataFrame([[i, m,n ] for i,j in t.to_dict().items() for m, n in j.items()],
                      columns=['actual', 'predict', 'over_values'])
-    heatmap = pe.HeatMap("Confusion Matrix Map")
-    heatmap.add("Confusion Matrix", t.drop_duplicates(['actual']).actual.values, t.drop_duplicates(['predict']).predict.values,
-                t.values, is_visualmap=True, xaxis_name='Actual', yaxis_name='Predict',
-                visual_text_color="#000", visual_orient="horizontal", visual_range=[0, t.over_values.max()])
-    return  heatmap if jupyter else heatmap.render(path)
+    heatmap = (pe.charts.HeatMap()
+               .add_xaxis(t.drop_duplicates(['actual']).actual.values.tolist())
+               .add_yaxis("Confusion Matrix", t.drop_duplicates(['predict']).predict.values.tolist(), t.values.tolist())
+               .set_global_opts(title_opts=pe.options.TitleOpts(title="Confusion Matrix Map"),
+                                xaxis_opts=pe.options.AxisOpts('Actual'),
+                                yaxis_opts=pe.options.AxisOpts('Predict'),
+                                visualmap_opts=pe.options.VisualMapOpts(min_=0, max_=int(t.over_values.max()))))
+    return  heatmap.render_notebook() if jupyter else heatmap.render(path)
