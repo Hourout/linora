1df26e649e86073435f22362434db44cd7d7cd50
JinQing Lee <hourout@163.com>
2019-06-04 15:39:59 +0800

    Update __init__.py

--- linora/__init__.py
+++ linora/__init__.py
@@ -8,16 +8,17 @@ from linora import sample_splits
 from linora import text
 from linora import image
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __author__ = 'JinQing Lee'
+
 def _hello():
     print("""
 ------------------------------------------------------------------------------------
       Linora
 --------------------
       Version      : --  {}  --
-      Author       : JinQing Lee
+      Author       : --  {}  --
       License      : Apache-2.0
       Homepage     : https://github.com/Hourout/linora
       Description  : Simple and efficient tools for data mining and data analysis.
-------------------------------------------------------------------------------------""".format(__version__))
+------------------------------------------------------------------------------------""".format(__version__, __author__))
