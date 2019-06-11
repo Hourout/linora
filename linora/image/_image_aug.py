5fcf41a7687ea7fff486974c7909573ffa043a7f
JinQing Lee <hourout@163.com>
2019-06-11 17:02:57 +0800

    Create _image_aug.py

---
+++ linora/image/_image_aug.py
@@ -0,0 +1,12 @@
+from linora.image._image_io_aug import ImageIoAug
+from linora.image._image_color_aug import ImageColorAug
+from linora.image._image_noise_aug import ImageNoiseAug
+from linora.image._image_resize_aug import ImageResizeAug
+from linora.image._image_rescale_aug import ImageRescaleAug
+from linora.image._image_position_aug import ImagePositionAug
+
+__all__ = ['ImageAug']
+
+class ImageAug(ImageIoAug, ImageColorAug, ImageNoiseAug, ImageResizeAug, ImageRescaleAug, ImagePositionAug):
+    def __init__(self, image=None):
+        super(IoAug, self).__init__(image)
