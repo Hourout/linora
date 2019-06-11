9909333bd59acd4dd64180a1ab04c0b744eb338d
JinQing Lee <hourout@163.com>
2019-06-11 11:09:20 +0800

    Update _imageaug.py

--- linora/image/_imageaug.py
+++ linora/image/_imageaug.py
@@ -1,6 +1,11 @@
 import numpy as np
 import tensorflow as tf
-from linora.image._image import *
+from linora.image._image_io import *
+from linora.image._image_color import *
+from linora.image._image_noise import *
+from linora.image._image_resize import *
+from linora.image._image_rescale import *
+from linora.image._image_position import *
 
 __all__ = ['ImageAug']
 
