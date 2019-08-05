00aa989bf852c4e14e91a01f1daec92ad059fcb8
JinQing Lee <hourout@163.com>
2019-08-05 17:02:00 +0800

    add doc

--- linora/feature_column/boundary/_boundary.py
+++ linora/feature_column/boundary/_boundary.py
@@ -1,11 +1,27 @@
 __all__ = ['uniform', 'quantile']
 
 def uniform(feature, bins):
+    """Equal width bin
+    
+    Args:
+        feature: pd.Series, model feature values.
+        bins: int, split bins of feature.
+    Returns:
+        the list of split threshold of feature.
+    """
     t = (feature.max()-feature.min())/bins
     m = feature.min()
     return [t*i+m for i in range(bins)]+[feature.max()]
 
 def quantile(feature, bins):
+    """Equal frequency bin
+    
+    Args:
+        feature: pd.Series, model feature values.
+        bins: int, split bins of feature.
+    Returns:
+        the list of split threshold of feature.
+    """
     t = feature.sort_values().values
     w = round(len(t)/bins)
     return [t[w*i] for i in range(bins)]+[feature.max()]
