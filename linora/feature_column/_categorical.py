8c23682975402515440a06a7218e392dd13dc736
JinQing Lee <hourout@163.com>
2019-08-06 15:10:29 +0800

    add doc

--- linora/feature_column/_categorical.py
+++ linora/feature_column/_categorical.py
@@ -10,17 +10,51 @@ __all__ = ['categorical_encoder', 'categorical_hash', 'categorical_crossed',
 
 
 def categorical_encoder(feature, feature_scale=None):
+    """Encode labels with value between 0 and n_classes-1.
+    
+    Args:
+        feature: pd.Series, sample feature.
+        feature_scale: dict, label parameters dict for this estimator.
+    Returns:
+        return encoded labels and label parameters dict.
+    """
     scale = feature_scale if feature_scale is not None else {j:i for i,j in feature.drop_duplicates().reset_index(drop=True).to_dict().items()}
     t = feature.replace(scale)
     return t, scale
 
 def categorical_hash(feature, hash_bucket_size):
+    """Hash labels with value between 0 and n_classes-1.
+    
+    Args:
+        feature: pd.Series, sample feature.
+        hash_bucket_size: int, number of categories that need hash.
+    Returns:
+        return hash labels.
+    """
     return feature.fillna('').astype(str).map(lambda x:hash(x))%hash_bucket_size
 
 def categorical_crossed(feature_list, hash_bucket_size):
+    """Crossed categories and hash labels with value between 0 and n_classes-1.
+    
+    Args:
+        feature_list: pd.Series list, sample feature list.
+        hash_bucket_size: int, number of categories that need hash.
+    Returns:
+        return hash labels.
+    """
     return reduce(lambda x,y:x+y, [i.fillna('').astype(str) for i in feature_list]).map(lambda x:hash(x))%hash_bucket_size
 
 def categorical_onehot_binarizer(feature, feature_scale=None, prefix='columns', dtype='int8'):
+    """Transform between iterable of iterables and a multilabel format, sample is simple categories.
+    
+    Args:
+        feature: pd.Series, sample feature.
+        feature_scale: list, feature categories list.
+        prefix: String to append DataFrame column names.
+        dtype: default np.uint8. Data type for new columns. Only a single dtype is allowed.
+    Returns:
+        Dataframe for onehot binarizer.
+    """
     assert not any(feature.isnull()), "`feature' should be not contains NaN"
     scale = feature.drop_duplicates().tolist()
     if feature_scale is not None:
@@ -38,6 +72,16 @@ def categorical_onehot_binarizer(feature, feature_scale=None, prefix='columns', 
     return t, scale
 
 def categorical_onehot_multiple(feature, feature_scale=None, prefix='columns', dtype='int8'):
+    """Transform between iterable of iterables and a multilabel format, sample is multiple categories.
+    
+    Args:
+        feature: pd.Series, sample feature.
+        feature_scale: list, feature categories list.
+        prefix: String to append DataFrame column names.
+        dtype: default np.uint8. Data type for new columns. Only a single dtype is allowed.
+    Returns:
+        Dataframe for onehot binarizer.
+    """
     assert not any(feature.isnull()), "`feature' should be not contains NaN."
     scale = feature_scale if feature_scale is not None else list(set(itertools.chain.from_iterable(feature)))
     class_mapping = defaultdict(int)
