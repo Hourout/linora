be51457a30a0f0a1dab7b073ba9146be9bc6aa78
JinQing Lee <hourout@163.com>
2019-08-08 17:11:12 +0800

    add doc

--- linora/metrics/distance/_distance.py
+++ linora/metrics/distance/_distance.py
@@ -6,35 +6,110 @@ __all__ = ['euclidean', 'manhattan', 'chebyshev', 'minkowski', 'hamming',
           ]
 
 def euclidean(x, y, normalize=False):
+    """euclidean distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+        normalize: default False, std=pd.concat([x, y]).std() if normalize else 1.
+    Returns:
+        euclidean distance value.
+    """
     assert isinstance(normalize, bool), "`normalize` should be bool types."
     std = pd.concat([x, y]).std() if normalize else 1
     return np.sqrt(np.sum(np.square((x-y)/std)))
 
 def manhattan(x, y):
+    """manhattan distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        manhattan distance value.
+    """
     return np.sum(np.abs(x-y))
 
 def chebyshev(x, y):
+    """chebyshev distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        chebyshev distance value.
+    """
     return np.max(x-y)
 
 def minkowski(x, y, p):
+    """minkowski distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+        p: int, norm dimension.
+    Returns:
+        minkowski distance value.
+    """
     return np.sqrt(np.sum(np.power(np.abs(x-y), p)))
 
 def hamming(x, y):
+    """hamming distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        hamming distance value.
+    """
     return len(x)-np.sum(np.equal(x, y))
 
 def jaccard(x, y):
+    """jaccard distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        jaccard distance value.
+    """
     return 1 - len(set(x).intersection(set(y)))/len(set(x).union(set(y)))
 
 def pearson(x, y):
+    """pearson distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        pearson distance value.
+    """
     x_mean, y_mean = np.mean(x), np.mean(y)
     d = np.sum((x-x_mean)*(y-y_mean))
     d2 = np.sqrt(np.sum(np.square(x-x_mean)))*np.sqrt(np.sum(np.square(y-y_mean)))
     return 1-d/d2
 
 def cosine(x, y):
+    """cosine distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+    Returns:
+        cosine distance value.
+    """
     return (x*y).sum()/np.sqrt(np.square(x).sum())/np.sqrt(np.square(y).sum())
 
 def levenshtein(x, y, normalize=False):
+    """levenshtein distance.
+    
+    Args:
+        x: pd.Series, sample feature value.
+        y: pd.Series, sample feature value.
+        normalize: default False, if normalize is True, levenshtein distance should be distance/max(len(x), len(y)).
+    Returns:
+        levenshtein distance value.
+    """
     def levenshtein1(s1, s2, normalize=False):
         if len(s1) < len(s2):
             return levenshtein1(s2, s1, normalize)
