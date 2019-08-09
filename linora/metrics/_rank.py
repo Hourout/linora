7d00cff618095da3b7ff89b5c2be280e1d486948
JinQing Lee <hourout@163.com>
2019-08-09 15:06:27 +0800

    add doc

--- linora/metrics/_rank.py
+++ linora/metrics/_rank.py
@@ -3,7 +3,15 @@ import pandas as pd
 __all__  = ['mapk', 'hit_ratio', 'mean_reciprocal_rank']
 
 def mapk(y_true, y_pred, k):
-    """Mean Average Precision k"""
+    """Mean Average Precision k
+    
+    Args:
+        y_true: pd.Series, ground truth (correct) labels.
+        y_pred: pd.Series, predicted values, as returned by a rank.
+        k: int, top k predict values.
+    Returns:
+        Mean Average Precision k values.
+    """
     def apk(actual, predict, k):
         if len(predict)>k:
             predict = predict[:k]
@@ -17,14 +25,30 @@ def mapk(y_true, y_pred, k):
     return pd.DataFrame({'label1':y_true, 'label2':y_pred}).apply(lambda x:apk(x[0], x[1], k=k), axis=1).mean()
 
 def hit_ratio(y_true, y_pred, k):
-    """Hit Ratio k"""
+    """Hit Ratio k
+    
+    Args:
+        y_true: pd.Series, ground truth (correct) labels.
+        y_pred: pd.Series, predicted values, as returned by a rank.
+        k: int, top k predict values.
+    Returns:
+        Hit Ratio k values.
+    """
     t = pd.DataFrame({'label1':y_true, 'label2':y_pred})
     a = t.apply(lambda x:len(set(x[0]).intersection(set(x[1]))) if len(x[1])<=k else len(set(x[0]).intersection(set(x[1][:k]))), axis=1).sum()
     b = t.label1.map(lambda x:len(set(x))).sum()
     return a/b
 
 def mean_reciprocal_rank(y_true, y_pred, k):
-    """Mean Reciprocal Rank"""
+    """Mean Reciprocal Rank
+    
+    Args:
+        y_true: pd.Series, ground truth (correct) labels.
+        y_pred: pd.Series, predicted values, as returned by a rank.
+        k: int, top k predict values.
+    Returns:
+        mean reciprocal rank k values.
+    """
     def mrr(actual, predict, k):
         if len(predict)>k:
             predict = predict[:k]
