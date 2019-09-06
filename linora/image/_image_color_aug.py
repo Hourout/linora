1c9a2fe8c7edc231fcc1657cba89dd0c5f32301e
JinQing Lee <hourout@163.com>
2019-09-06 18:11:54 +0800

    add RandomPencilSketch()

--- linora/image/_image_color_aug.py
+++ linora/image/_image_color_aug.py
@@ -128,3 +128,22 @@ class ImageColorAug(object):
         """
         self._image = RandomGamma(self._image, gamma, seed, _=True)
         return self
+
+    def RandomPencilSketch(image, delta=0.1, seed=None, **kwarg):
+    """Adjust the pencil sketch of RGB.
+    
+    Args:
+        image: Tensor or array. An image.
+        delta: if int, float, Amount to add to the pixel values.
+               if list, tuple, randomly picked in the interval
+               `[delta[0], delta[1])` to add to the pixel values.
+               a suitable interval is [0.1, 0.5].
+        seed: A Python integer. Used to create a random seed. See
+             `tf.set_random_seed` for behavior.
+    Returns:
+        A pencil_sketch-adjusted tensor of the same shape as `image`.
+    """
+        self._image = RandomPencilSketch(self._image, delta, seed, _=True)
+        return self
+    
+    
