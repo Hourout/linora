98c16fac7fd4a722f9034539a4e14ff13b9021fe
JinQing Lee <hourout@163.com>
2019-12-23 21:24:19 +0800

    add new method

--- linora/database/_hive.py
+++ linora/database/_hive.py
@@ -21,12 +21,21 @@ class HiveEngine():
         sql = f"CREATE TABLE IF NOT EXISTS {table} ({t}) row format delimited fields terminated by '{sep}'"
         return pd.io.sql.execute(sql, self.engine)
     
-    def alter_table_property(self, table, comment='', property_name=''):
+    def drop_table(self, table):
+        sql = f"DROP TABLE IF EXISTS {table}"
+        return pd.io.sql.execute(sql, self.engine)
+    
+    def alter_table_comment(self, table, comment='', property_name=''):
         sql = f"ALTER TABLE {table} SET TBLPROPERTIES('comment'='{comment}', 'property_name'='{property_name}')"
         return pd.io.sql.execute(sql, self.engine)
     
-    def drop_table(self, table):
-        return pd.io.sql.execute(f"DROP TABLE IF EXISTS {table}", self.engine)
+    def alter_table_delimiter(self, table, delimiter):
+        sql = f"ALTER TABLE {table} SET SERDEPROPERTIES('field.delim'='{delimiter}')"
+        return pd.io.sql.execute(sql, self.engine)
+    
+    def alter_table_name(self, old_table, new_table):
+        sql = f"ALTER TABLE {old_table} RENAME TO {new_table}"
+        return pd.io.sql.execute(sql, self.engine)
     
     def alter_column(self, table, old_column, new_column, dtype, comment):
         sql = f"ALTER TABLE {table} CHANGE COLUMN {old_column} {new_column} {dtype} COMMENT '{comment}'"
