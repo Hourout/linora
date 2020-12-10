072384a7ef57b898413dc7f9dc83c783c0025e64
JinQing Lee <hourout@163.com>
2020-12-10 16:04:11 +0800

    Update _image_io_aug.py

--- linora/image/_image_io_aug.py
+++ linora/image/_image_io_aug.py
@@ -1,53 +1,48 @@
 import numpy as np
-import tensorflow as tf
 from linora.image._image_io import *
 
 class ImageIoAug(object):
     def __init__(self, image=None):
-        self._image = image
+        self.image = image
     
-    def read_image(self, filename, channel=0, image_format='mix'):
-        """Convenience function for read image type one of `bmp`, `gif`, `jpeg`, `jpg`, and `png`.
-    
-        Detects whether an image is a BMP, GIF, JPEG, JPG, or PNG, and performs the
-        appropriate operation to convert the input bytes `string` into a `Tensor`
-        of type `dtype`.
-        Note: `gif` returns a 4-D array `[num_frames, height, width, 3]`, as
-        opposed to `bmp`, `jpeg`, `jpg` and `png`, which return 3-D
-        arrays `[height, width, num_channels]`. Make sure to take this into account
-        when constructing your graph if you are intermixing GIF files with BMP, JPEG, JPG,
-        and/or PNG files.
-        Args:
-            filename: 0-D `string`. image absolute path.
-            channels: An optional `int`. Defaults to `0`. Number of color channels for
-                      the decoded image. 1 for `grayscale` and 3 for `rgb`.
-            image_format: 0-D `string`. image format type one of `bmp`, `gif`, `jpeg`,
-                          `jpg`, `png` and `mix`. `mix` mean contains many types image format.
-        Returns:
-            `Tensor` with type uint8 and shape `[height, width, num_channels]` for
-            BMP, JPEG, and PNG images and shape `[num_frames, height, width, 3]` for
-            GIF images.
-        Raises:
-            ValueError: On incorrect number of channels.
+    def read_image(self, filename):
         """
-        self._image = read_image(filename, channel, image_format, _=True)
+        Args:
+            filename: str, image absolute path.
+        Returns:
+            a Image instance.
+        """
+        self.image = read_image(filename)
         return self
     
-    def save_image(self, filename):
-        """Writes image to the file at input filename. 
-    
-        Args:
-            image:    A Tensor of type string. scalar. The content to be written to the output file.
-            filename: A string. scalar. The name of the file to which we write the contents.
-        Raises:
-            ValueError: If `filename` is not in `[`jpg`, `jpeg`, `png`]`.
+    def save_image(self, filename, file_format=None, **kwargs):
+        """Saves an image stored as a Numpy array to a path or file object.
+        Args
+            filename: Path or file object.
+            image: A PIL Image instance.
+            file_format: Optional file format override. If omitted, the
+                format to use is determined from the filename extension.
+                If a file object was used instead of a filename, this
+                parameter should always be used.
+            **kwargs: Additional keyword arguments passed to `PIL.Image.save()`.
         """
-        return save_image(self._image, filename)
+        return save_image(filename, self.image, file_format=file_format, **kwargs)
     
-    def run(self):
-        """return numpy array image."""
-        return self._image if type(self._image)==np.ndarray else self._image.numpy()
+    def color_convert(self, color_mode='RGB'):
+        self.image = color_convert(self.image, color_mode=color_mode)
+        return self
     
-    def show(self):
-        """plot numpy array image."""
-        return tf.keras.preprocessing.image.array_to_img((self._image if type(self._image)==np.ndarray else self._image.numpy()).astype('uint8'))
+    def image_to_array(self, data_format='channels_last', dtype='float32'):
+        """Converts a PIL Image instance to a Numpy array.
+        # Arguments
+            img: PIL Image instance.
+            data_format: Image data format,
+                either "channels_first" or "channels_last".
+            dtype: Dtype to use for the returned array.
+        # Returns
+            A 3D Numpy array.
+        # Raises
+            ValueError: if invalid `img` or `data_format` is passed.
+        """
+        return image_to_array(self.image, data_format=data_format, dtype=dtype)
+        
