8e37ff066a8a7a2d4e5c60117d918c92fc4c6bf4
JinQing Lee <hourout@163.com>
2020-12-15 11:16:07 +0800

    Update _image_resize_aug.py

--- linora/image/_image_resize_aug.py
+++ linora/image/_image_resize_aug.py
@@ -5,6 +5,15 @@ class ImageResizeAug(object):
         self.image = image
         
     def resize(self, size, method=ResizeMethod.BILINEAR):
+        """Returns a resized copy of this image.
+    
+        Args:
+        size: The requested size in pixels, as a 2-tuple: (width, height).
+        method: An optional resampling filter. see la.image.ResizeMethod.
+
+        Returns:
+            a Image instance.
+        """
         self.image = resize(self.image, size, method)
         return self
     
