57027deba4a237e512c4ca92e35dd6d3ce35150b
JinQing Lee <hourout@163.com>
2021-01-11 14:13:59 +0800

    Update _random_walker.py

--- linora/sample/_random_walker.py
+++ linora/sample/_random_walker.py
@@ -16,33 +16,37 @@ class RandomWalker:
         self.p = p
         self.q = q
 
-    def deepwalk_walks(self, walk_num, walk_length, filter_lenth=0, workers=1, verbose=0):
-        return self._parallel_walks(self._deepwalk_walks, walk_num, walk_length, filter_lenth, workers, verbose)
+    def deepwalk_walks(self, walk_num, walk_length, walk_prob=False, filter_lenth=0, workers=1, verbose=0):
+        return self._parallel_walks(self._deepwalk_walks, walk_num, walk_length, walk_prob, filter_lenth, workers, verbose)
         
-    def deepwalk_walk(self, walk_length, start_node, filter_lenth=0):
+    def deepwalk_walk(self, walk_length, start_node, walk_prob=False, filter_lenth=0):
         walk = [start_node]
         while len(walk) < walk_length:
             cur = walk[-1]
-            cur_nbrs = list(self.G.neighbors(cur))
+            cur_nodes = list(self.G.neighbors(cur))
             if len(cur_nbrs) > 0:
-                walk.append(random.choice(cur_nbrs))
+                if walk_prob:
+                    cur_weights = [G[cur][i]['weight'] for i in cur_nodes]
+                    walk.append(random.choices(cur_nodes, cur_weights)[0])
+                else:
+                    walk.append(random.choice(cur_nbrs))
             else:
                 break
         if len(walk)<=filter_lenth:
             walk = []
         return walk
     
-    def _deepwalk_walks(self, nodes, walk_num, walk_length, filter_lenth):
+    def _deepwalk_walks(self, nodes, walk_num, walk_length, walk_prob, filter_lenth):
         walks = []
         for _ in range(walk_num):
             random.shuffle(nodes)
-            walks += [self.deepwalk_walk(walk_length, v, filter_lenth) for v in nodes]
+            walks += [self.deepwalk_walk(walk_length, v, walk_prob, filter_lenth) for v in nodes]
         return walks
     
-    def _parallel_walks(self, method, walk_num, walk_length, filter_lenth, workers, verbose):
+    def _parallel_walks(self, method, walk_num, walk_length, walk_prob, filter_lenth, workers, verbose):
         nodes = list(self.G.nodes())
         workers_list = [walk_num//workers]*workers + ([walk_num % workers] if walk_num % workers != 0 else [])
         results = Parallel(n_jobs=workers, verbose=verbose)(
-            delayed(method)(nodes, num, walk_length, filter_lenth) for num in workers_list)
+            delayed(method)(nodes, num, walk_length, walk_prob, filter_lenth) for num in workers_list)
         walks = itertools.chain(filter(lambda x: len(x) > 0, itertools.chain(*results)))
         return walks
