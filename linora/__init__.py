842e8f84ad86538b9ff0e5d25d699a53b2b8c877
JinQing Lee <hourout@163.com>
2021-01-15 17:52:53 +0800

    Update __init__.py

--- linora/__init__.py
+++ linora/__init__.py
@@ -9,6 +9,7 @@ from linora import text
 from linora import image
 # from linora import database
 from linora._logger import *
+from linora._config import *
 
 __version__ = '1.0.0rc1'
 __author__ = 'JinQing Lee'
