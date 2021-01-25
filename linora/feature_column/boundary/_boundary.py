206d3b66384c0602140da1d08d026a7d3f36c720
JinQing Lee <hourout@163.com>
2021-01-25 11:16:27 +0800

    Update _boundary.py

--- linora/feature_column/boundary/_boundary.py
+++ linora/feature_column/boundary/_boundary.py
@@ -1,7 +1,7 @@
 __all__ = ['uniform', 'quantile']
 
 def uniform(feature, bins):
-    """Equal width bin
+    """Equal width bin, take a uniform distribution for the sample value range.
     
     Args:
         feature: pd.Series, model feature values.
@@ -14,7 +14,7 @@ def uniform(feature, bins):
     return [t*i+m for i in range(bins)]+[feature.max()]
 
 def quantile(feature, bins):
-    """Equal frequency bin
+    """Equal frequency bin, take a uniform distribution of the sample size.
     
     Args:
         feature: pd.Series, model feature values.
