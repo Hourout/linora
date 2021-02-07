231179d52751ee599aeed36120638395fd95b121
JinQing Lee <hourout@163.com>
2021-02-07 16:20:01 +0800

    Update __init__.py

--- linora/metrics/__init__.py
+++ linora/metrics/__init__.py
@@ -2,3 +2,4 @@ from linora.metrics import distance
 from linora.metrics._regression import *
 from linora.metrics._classification import *
 from linora.metrics._rank import *
+from linora.metrics._metrics import *
