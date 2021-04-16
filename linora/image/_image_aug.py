53382d921b32e49700f68af4dffc340948819b7d
JinQing Lee <hourout@163.com>
2021-04-16 10:57:06 +0800

    Update _image_aug.py

--- linora/image/_image_aug.py
+++ linora/image/_image_aug.py
@@ -5,9 +5,10 @@ from linora.image._image_resize_aug import ImageResizeAug
 from linora.image._image_rescale_aug import ImageRescaleAug
 from linora.image._image_position_aug import ImagePositionAug
 from linora.image._image_crop_aug import ImageCropAug
+from linora.image._image_filter_aug import ImageFilterAug
 
 __all__ = ['ImageAug']
 
-class ImageAug(ImageIoAug, ImageColorAug, ImageNoiseAug, ImageResizeAug, ImageRescaleAug, ImagePositionAug, ImageCropAug):
+class ImageAug(ImageIoAug, ImageColorAug, ImageNoiseAug, ImageResizeAug, ImageRescaleAug, ImagePositionAug, ImageCropAug, ImageFilterAug):
     def __init__(self, image=None):
         super(ImageAug, self).__init__(image)
