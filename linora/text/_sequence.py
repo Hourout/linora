3cf9e410e7dabd610426b6c09acf5a4e932f27ba
JinQing Lee <hourout@163.com>
2021-06-09 10:18:50 +0800

    add new api la.text.make_sampling_table

--- linora/text/_sequence.py
+++ linora/text/_sequence.py
@@ -1,6 +1,8 @@
 import random
 
-__all__ = ['skipgrams', 'pad_sequences']
+import numpy as np
+
+__all__ = ['skipgrams', 'pad_sequences', 'make_sampling_table']
 
 def skipgrams(sequence, vocabulary_size,
               window_size=4, negative_samples=1., shuffle=True,
@@ -99,3 +101,33 @@ def pad_sequences(sequence, maxlen, dtype='int32', padding='pre', truncating='pr
     else:
         raise ValueError('Padding type "%s" not understood or Truncating type "%s" not understood' % (padding, truncating))
     return t
+
+def make_sampling_table(size, sampling_factor=1e-5):
+    """Generates a word rank-based probabilistic sampling table.
+    Used for generating the `sampling_table` argument for `skipgrams`.
+    `sampling_table[i]` is the probability of sampling
+    the word i-th most common word in a dataset
+    (more common words should be sampled less frequently, for balance).
+    The sampling probabilities are generated according
+    to the sampling distribution used in word2vec:
+    ```
+    p(word) = (min(1, sqrt(word_frequency / sampling_factor) /
+        (word_frequency / sampling_factor)))
+    ```
+    We assume that the word frequencies follow Zipf's law (s=1) to derive
+    a numerical approximation of frequency(rank):
+    `frequency(rank) ~ 1/(rank * (log(rank) + gamma) + 1/2 - 1/(12*rank))`
+    where `gamma` is the Euler-Mascheroni constant.
+    # Arguments
+        size: Int, number of possible words to sample.
+        sampling_factor: The sampling factor in the word2vec formula.
+    # Returns
+        A 1D Numpy array of length `size` where the ith entry
+        is the probability that a word of rank i should be sampled.
+    """
+    gamma = 0.577
+    rank = np.arange(size)
+    rank[0] = 1
+    inv_fq = rank * (np.log(rank) + gamma) + 0.5 - 1. / (12. * rank)
+    f = sampling_factor * inv_fq
+    return np.minimum(1., f / np.sqrt(f))
