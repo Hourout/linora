362b24efe2975855750427cf07d1acef9566e834
JinQing Lee <hourout@163.com>
2021-06-16 14:49:19 +0800

    Update __init__.py

--- linora/param_search/__init__.py
+++ linora/param_search/__init__.py
@@ -2,7 +2,7 @@
 # from linora.param_search import GERegressor
 # from linora.param_search import LGBClassifier
 from linora.param_search import XGBClassifier
-from linora.param_search import XGBRanker
+# from linora.param_search import XGBRanker
 from linora.param_search import XGBRegressor
 
 from linora.param_search._HyperParameters import *
