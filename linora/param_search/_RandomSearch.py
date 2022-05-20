399325fd2593d8606c7d4e09cd3efd7a58710a78
JinQing Lee <hourout@163.com>
2022-05-20 11:36:45 +0800

    new version 1.4.0

--- linora/param_search/_RandomSearch.py
+++ linora/param_search/_RandomSearch.py
@@ -1,5 +1,6 @@
 import os
 import json
+from collections import Counter
 
 import numpy as np
 import pandas as pd
@@ -7,15 +8,24 @@ import pandas as pd
 from linora.utils._config import Config
 from linora.utils._logger import Logger
 from linora.sample._fold import kfold, train_test_split
+from linora.param_search._config import model_hp
 from linora.param_search._HyperParameters import HyperParametersRandom
+
+__all__ = ['RandomSearch']
 
 
 class RandomSearch():
-    def __init__(self, hp, model, name=''):
+    def __init__(self, model=None, hp=None, name=''):
         self.params = Config()
         self.params.name = name
         self.params.model_init = model
-        self.HyperParameter = hp
+        
+        if model in ['XGBClassifier', 'XGBRegressor', 'LGBMClassifier', 'LGBMRegressor']:
+            self.hp = model_hp(model=model, method='random')
+            if hp is not None:
+                self.hp.from_HyperParameters(hp)
+        else:
+            self.hp = hp
         self.best_params = dict()
         self.best_params_history = dict()
 
@@ -23,7 +33,7 @@ class RandomSearch():
                iter_num=100, scoring=0.5, cv=5, cv_num=3, metrics_min=True, 
                speedy=True, speedy_param=(20000, 0.3), 
                save_model_dir=None, save_model_name=None):
-        """XGBRegressor model params search use RandomSearch method.
+        """model params search use RandomSearch method.
 
         Args:
             train_data: A list of (X, y, sample_weight) tuple pairs to use as train sets.
@@ -37,8 +47,8 @@ class RandomSearch():
             speedy: whether use speedy method.
             speedy_param: if use speedy method, test_size will be set, 
                           test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2).
-            save_model_dir: str, save model folder.
-            save_model_name: str, save model name prefix.
+            save_model_dir: str, save model folder, only work with model='XGBClassifier' or 'XGBRegressor'.
+            save_model_name: str, save model name prefix, only work with model='XGBClassifier' or 'XGBRegressor'.
         Returns:
             a best model params dict.
         Raises:
@@ -49,14 +59,16 @@ class RandomSearch():
         warnings.filterwarnings("ignore")
         if speedy:
             test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2)
-
+        if self.params.model_init=='XGBClassifier':
+            self._xgb_weight(train_data[1])
+        
         if vaild_data is not None:
             cv_score_list = []
             
         logger.info(f"Start {self.name} hyperparameter random search.")
         for i in range(1, iter_num+1):
-            self.HyperParameter.update()
-            self.params.model = self.params.model_init(**self.HyperParameter.params)
+            self.hp.update()
+            self.params.model = self.params.model_init(**self.hp.params)
             score = []
             if speedy:
                 for _ in range(cv_num):
@@ -78,14 +90,15 @@ class RandomSearch():
                     continue
             if (metrics_min==True and cv_score<scoring) or (metrics_min==False and cv_score>scoring):
                 scoring = cv_score
-                self.best_params = self.HyperParameter.params.copy()
+                self.best_params = self.hp.params.copy()
                 self.best_params_history[i] = {'score':scoring, 'best_params':self.best_params.copy()}
-                if save_model_dir is not None:
-                    if save_model_name is None:
-                        save_model_name = 'RS'
-#                     model.save_model(os.path.join(save_model_dir, f"{save_model_name}_model.json"))
-#                     with open(os.path.join(save_model_dir, f"{save_model_name}_params.json"),'w') as f:
-#                         json.dump(best_params, f)
+                if self.params.model_init in ['XGBClassifier', 'XGBRegressor']:
+                    if save_model_dir is not None:
+                        if save_model_name is None:
+                            save_model_name = 'RS'
+                        model.save_model(os.path.join(save_model_dir, f"{save_model_name}_model.json"))
+                        with open(os.path.join(save_model_dir, f"{save_model_name}_params.json"),'w') as f:
+                            json.dump(best_params, f)
             logger.info(f"Random search progress: {i/iter_num*100:.1f}%, best score: {scoring:.4f}", enter=False if i<iter_num else True)
         logger.info(f"{self.name} random search best score: {scoring:.4f}", close=True, time_mode=1)
         return self.best_params
@@ -110,3 +123,12 @@ class RandomSearch():
                 return metrics(data[1], cv_pred, sample_weight=data[2])
             else:
                 return metrics(data[1][index[1]], cv_pred, sample_weight=data[2][index[1]])
+    
+    def _xgb_weight(self, label):
+        weight_dict = Counter(label)
+        if len(weight_dict)==2:
+            weight = int(np.ceil(weight_dict[min(weight_dict)]/weight_dict[max(weight_dict)]))
+        else:
+            weight_dict = {j:i for i,j in weight_dict.items()}
+            weight = int(np.ceil(weight_dict[max(weight_dict)]/weight_dict[min(weight_dict)]))
+        self.hp.Choice('scale_pos_weight', [1, weight])
--- linora/param_search/__init__.py
+++ linora/param_search/__init__.py
@@ -7,6 +7,7 @@ from linora.param_search import XGBRegressor
 # from linora.param_search import XGBRanker
 
 from linora.param_search._HyperParameters import *
+from linora.param_search._RandomSearch import *
 
 from linora.param_search._config import __xgboost_version__, __lightgbm_version__
 
