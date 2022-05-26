06a8350fe4e4289fa7c9333984c8ce7026573525
JinQing Lee <hourout@163.com>
2022-05-26 18:51:12 +0800

    new version 1.4.0

--- linora/data/__init__.py
+++ linora/data/__init__.py
@@ -2,5 +2,5 @@
 # from linora.data._text_line_dataset import *
 
 from linora.data import Dataset
-from linora.data import ImageDataset
-from linora.data import TextLineDataset
+# from linora.data import ImageDataset
+# from linora.data import TextLineDataset
