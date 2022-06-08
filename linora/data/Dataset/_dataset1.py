2938d7e11709cf9f8e3a3e9d80d09aff141eb456
JinQing Lee <hourout@163.com>
2022-06-08 15:38:59 +0800

    new version 1.4.0

--- linora/data/Dataset/_dataset1.py
+++ linora/data/Dataset/_dataset1.py
@@ -28,7 +28,7 @@ class BatchFunction():
                 continue
             if isinstance(t[i]['output'], (list, tuple)):
                 print(t)
-                print(([np.expand_dims(t[j]['output'][k], 0) for j in t if not t[j]['got_error']] for k in range(len(t[i]['output'])))
+                print([[np.expand_dims(t[j]['output'][k], 0) for j in t if not t[j]['got_error']] for k in range(len(t[i]['output']))])
                 return [np.concatenate([np.expand_dims(t[j]['output'][k], 0) for j in t if not t[j]['got_error']]) for k in range(len(t[i]['output']))]
             else:
                 return np.concatenate([np.expand_dims(t[j]['output'], 0) for j in t if not t[j]['got_error']])
