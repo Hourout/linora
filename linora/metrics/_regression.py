9742be2992689e5b178f2f25d6d50b655b5a8d33
JinQing Lee <hourout@163.com>
2022-06-22 14:25:21 +0800

    new version 1.5.0

--- linora/metrics/_regression.py
+++ linora/metrics/_regression.py
@@ -5,7 +5,8 @@ from linora.metrics._utils import _sample_weight
 __all__ = ['normal_loss', 'mean_absolute_error', 'mean_squared_error',
            'mean_absolute_percentage_error', 'hinge', 'explained_variance_score',
            'median_absolute_error', 'r2_score', 'report_regression',
-           'mean_relative_error', 'poisson', 'log_cosh_error'
+           'mean_relative_error', 'poisson', 'log_cosh_error', 'max_error',
+           'mean_tweedie_deviance', 'mean_poisson_deviance', 'mean_gamma_deviance'
           ]
 
 
@@ -79,6 +80,18 @@ def mean_absolute_percentage_error(y_true, y_pred, sample_weight=None):
     value = np.abs(y_pred - y_true)
     y_true[np.where(y_true==0)] = 1
     return (value/np.abs(y_true)*sample_weight).mean()
+
+
+def max_error(y_true, y_pred):
+    """The max_error metric calculates the maximum residual error.
+    
+    Args:
+        y_true: pd.Series or array or list, ground truth (correct) labels.
+        y_pred: pd.Series or array or list, predicted values, as returned by a regression.
+    Returns:
+        regression loss values.
+    """
+    return np.max(np.abs(np.array(y_true) - np.array(y_pred)))
 
 
 def hinge(y_true, y_pred, k=1, sample_weight=None):
@@ -224,3 +237,84 @@ def log_cosh_error(y_true, y_pred, sample_weight=None):
     x = (y_pred-y_true)*sample_weight
     sample_weight = _sample_weight(y_true, sample_weight)
     return np.mean(x + np.log(np.exp(-2. * x) + 1.) - np.log(2.))
+
+
+def mean_tweedie_deviance(y_true, y_pred, p, sample_weight=None):
+    """Mean Tweedie deviance regression loss.
+    
+    when p=0 it is equivalent to mean_squared_error.
+    when p=1 it is equivalent to mean_poisson_deviance.
+    when p=2 it is equivalent to mean_gamma_deviance.
+
+    p < 0: Extreme stable distribution. Requires: y_pred > 0.
+    p = 0 : Normal distribution, output corresponds to mean_squared_error. y_true and y_pred can be any real numbers.
+    p = 1 : Poisson distribution. Requires: y_true >= 0 and y_pred > 0.
+    1 < p < 2 : Compound Poisson distribution. Requires: y_true >= 0 and y_pred > 0.
+    p = 2 : Gamma distribution. Requires: y_true > 0 and y_pred > 0.
+    p = 3 : Inverse Gaussian distribution. Requires: y_true > 0 and y_pred > 0.
+    otherwise : Positive stable distribution. Requires: y_true > 0 and y_pred > 0.
+
+    Args:
+        y_true: pd.Series or array or list, ground truth (correct) labels.
+        y_pred: pd.Series or array or list, predicted values, as returned by a regression.
+        p: Tweedie power parameter. Either p <= 0 or p >= 1.
+           The higher p the less weight is given to extreme deviations between true and predicted targets.
+        sample_weight: list or array of sample weight.
+    Returns:
+        A non-negative floating point value (the best value is 0.0).
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    if p < 0:
+        if (y_pred <= 0).any():
+            raise ValueError("p < 0: Extreme stable distribution. Requires: y_pred > 0.")
+    elif p == 0:
+        pass
+    elif 0 < p < 1:
+        raise ValueError("Tweedie deviance is only defined for p<=0 and p>=1.")
+    elif 1 <= p < 2:
+        if (y_true < 0).any() or (y_pred <= 0).any():
+            raise ValueError("1 < p < 2 : Compound Poisson distribution. Requires: y_true >= 0 and y_pred > 0.")
+    else:
+        if (y_true <= 0).any() or (y_pred <= 0).any():
+            raise ValueError("p>=2, Positive stable distribution. Requires: y_true > 0 and y_pred > 0.")
+
+    if p==0:
+        t = np.square(y_true-y_pred)
+    elif p==1:
+        t = 2*(y_true*np.log(y_true/y_pred)+y_pred-y_true)
+    elif p==2:
+        t = 2*(np.log(y_pred/y_true)+y_true/y_pred - 1)
+    else:
+        t = 2 * (
+            np.power(np.maximum(y_true, 0), 2 - p) / ((1 - p) * (2 - p))
+            - y_true * np.power(y_pred, 1 - p) / (1 - p)
+            + np.power(y_pred, 2 - p) / (2 - p))
+    sample_weight = _sample_weight(y_true, sample_weight)
+    return np.average(t, weights=sample_weight)
+
+
+def mean_poisson_deviance(y_true, y_pred, sample_weight=None):
+    """Mean Poisson deviance regression loss.
+    
+    Args:
+        y_true: pd.Series or array or list, ground truth (correct) labels.
+        y_pred: pd.Series or array or list, predicted values, as returned by a regression.
+        sample_weight: list or array of sample weight.
+    Returns:
+        A non-negative floating point value (the best value is 0.0).
+    """
+    return mean_tweedie_deviance(y_true, y_pred, p=1, sample_weight=sample_weight)
+
+
+def mean_gamma_deviance(y_true, y_pred, sample_weight=None):
+    """Mean Gamma deviance regression loss.
+
+    Args:
+        y_true: pd.Series or array or list, ground truth (correct) labels.
+        y_pred: pd.Series or array or list, predicted values, as returned by a regression.
+        sample_weight: list or array of sample weight.
+    Returns:
+        A non-negative floating point value (the best value is 0.0).
+    """
+    return mean_tweedie_deviance(y_true, y_pred, p=2, sample_weight=sample_weight)
