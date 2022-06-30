a4473977bf7ff47839df5a38e2eaea45437aaaab
JinQing Lee <hourout@163.com>
2022-06-30 18:42:15 +0800

    new version 1.5.0

--- linora/chart/__init__.py
+++ linora/chart/__init__.py
@@ -1,4 +1,6 @@
 from linora.chart._metrics import *
 from linora.chart._feature import *
 
+from linora.chart._config import *
+
 from linora.chart._line import *
---
+++ linora/chart/_config.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from linora.utils._config import Config
+
+__all__ = ['Options']
+
+
+Options = Config()
+Options.theme = plt.style.available
