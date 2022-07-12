e9db33f2858466118fc8ff62b47ec413a1a48c13
JinQing Lee <hourout@163.com>
2022-07-12 17:58:29 +0800

    new version 1.5.0

--- linora/chart/__init__.py
+++ linora/chart/__init__.py
@@ -2,10 +2,11 @@
 # from linora.chart._feature import *
 
 from linora.chart._config import *
+from linora.chart._plot import *
 
-from linora.chart._errorbar import *
-from linora.chart._fillline import *
-from linora.chart._hist import *
-from linora.chart._hist2d import *
-from linora.chart._line import *
-from linora.chart._scatter import *
+# from linora.chart._errorbar import *
+# from linora.chart._fillline import *
+# from linora.chart._hist import *
+# from linora.chart._hist2d import *
+# from linora.chart._line import *
+# from linora.chart._scatter import *
