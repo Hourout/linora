5ff4adb6c0180f6964f22c8356a5efe8feb59244
JinQing Lee <hourout@163.com>
2022-07-25 15:52:04 +0800

    new version 1.5.0

--- linora/chart/_scatter.py
+++ linora/chart/_scatter.py
@@ -90,7 +90,7 @@ class Scatter():
         self._params.ydata[name]['plotfunc'] = self._execute_plot
         return self
     
-    def _execute_plot(self, ax, i j):
+    def _execute_plot(self, ax, i, j):
         ax_plot = ax.scatter(j['xdata'], j['ydata'], **j['kwargs'])
         ax_plot.set_label(i)
         
