60eb8c8228dcc3acc8ffb3acc1d1917a4428b355
JinQing Lee <hourout@163.com>
2022-08-02 16:00:04 +0800

    new version 1.5.0

---
+++ linora/chart/_hlines.py
@@ -0,0 +1,24 @@
+class Hlines():
+    def add_hlines(self, name, ydata, xmin, xmax, **kwargs):
+        """Plot y versus x as lines and/or markers.
+        
+        Args:
+            name: data name.
+            ydata: y-axis data, float or array-like.
+            xmin, xmax : float or array-like
+                Respective beginning and end of each line. If scalars are
+                provided, all lines will have same length.
+            colors : list of colors, default: :rc:`lines.color`
+            linestyles : {'solid', 'dashed', 'dashdot', 'dotted'}, optional
+            label : str, default: ''
+        """
+        self._params.ydata[name]['kwargs'] = kwargs
+        self._params.ydata[name]['xmin'] = xmin
+        self._params.ydata[name]['xmax'] = xmax
+        self._params.ydata[name]['ydata'] = ydata
+        self._params.ydata[name]['plotmode'] = 'hlines'
+        self._params.ydata[name]['plotfunc'] = self._execute_plot_hlines
+        return self
+    
+    def _execute_plot_hlines(self, ax, i, j):
+        ax_plot = ax.hlines(j['ydata'], j['xin'], j['xmax'], **j['kwargs'])
