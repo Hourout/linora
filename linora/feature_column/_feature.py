d8b4a27aed253c30ce41e490b175c547d95c845d
JinQing Lee <hourout@163.com>
2023-03-20 15:04:13 +0800

    new version 2.0.0

--- linora/feature_column/_feature.py
+++ linora/feature_column/_feature.py
@@ -16,55 +16,40 @@ class Feature(FeatureCategorical, FeatureNumerical, FeatureNormalize):
     def __init__(self):
         super(Feature, self).__init__()
         self._params = Config()
-        self._params.function = [
-            ('categorical_count', categorical_count), ('categorical_crossed', categorical_crossed), 
-            ('categorical_encoder', categorical_encoder), ('categorical_hash', categorical_hash), 
-            ('categorical_hist', categorical_hist), ('categorical_regress', categorical_regress),
-            ('categorical_onehot_binarizer', categorical_onehot_binarizer),
-            ('categorical_onehot_multiple', categorical_onehot_multiple),
-            ('categorical_rare', categorical_rare), ('categorical_regress', categorical_regress),
-            ('categorical_woe', categorical_woe),
-            ('numerical_binarizer', numerical_binarizer), ('numerical_bucketized', numerical_bucketized),
-            ('numerical_padding', numerical_padding), ('numerical_outlier', numerical_outlier),
-            ('normalize_max', normalize_max), ('normalize_maxabs', normalize_maxabs), 
-            ('normalize_l1', normalize_l1), ('normalize_l2', normalize_l2),
-            ('normalize_meanminmax', normalize_meanminmax), ('normalize_minmax', normalize_minmax),
-            ('normalize_norm', normalize_norm), ('normalize_robust', normalize_robust)
-           ]
+        self._params.function = {
+            'categorical_count':categorical_count, 
+            'categorical_crossed':categorical_crossed, 
+            'categorical_encoder':categorical_encoder, 
+            'categorical_hash':categorical_hash, 
+            'categorical_hist':categorical_hist,
+            'categorical_regress':categorical_regress,
+            'categorical_onehot_binarizer':categorical_onehot_binarizer,
+            'categorical_onehot_multiple':categorical_onehot_multiple,
+            'categorical_rare':categorical_rare,
+            'categorical_regress':categorical_regress,
+            'categorical_woe':categorical_woe,
+            'numerical_binarizer':numerical_binarizer,
+            'numerical_bucketized':numerical_bucketized,
+            'numerical_padding':numerical_padding, 
+            'numerical_outlier':numerical_outlier,
+            'normalize_max':normalize_max, 
+            'normalize_maxabs':normalize_maxabs, 
+            'normalize_l1':normalize_l1, 
+            'normalize_l2':normalize_l2,
+            'normalize_meanminmax':normalize_meanminmax,
+            'normalize_minmax', normalize_minmax,
+            'normalize_norm':normalize_norm, 
+            'normalize_robust':normalize_robust
+        }
         self.pipe = {}
-        
-#     def categorical_count(self, variable, normalize=True, abnormal_value=0, miss_value=0, name=None, keep=False):
-#         """Count or frequency of conversion category variables.
-    
-#         Args:
-#             variable: str, feature variable name.
-#             normalize: bool, If True then the object returned will contain the relative frequencies of the unique values.
-#             abnormal_value: int or float, if feature values not in feature_scale dict, return `abnormal_value`.
-#             miss_value: int or float, if feature values are missing, return `miss_value`.
-#             name: str, output feature name, if None, name is variable.
-#             keep: if name is not None, name whether to keep in the final output.
-#         Returns:
-#             return count labels and label parameters dict.
-#         """
-#         config = {'param':{'normalize':normalize, 'abnormal_value':abnormal_value, 
-#                            'miss_value':miss_value, 'name':variable if name is None else name},
-#                   'type':'categorical_count', 'variable':variable, 'keep':keep}
-#         self.pipe[len(self.pipe)] = config
-#         return self
-        
-#     def add_pipe(self, config):
-#         self.pipe[len(self.pipe)] = config[0] if isinstance(config, tuple) else config
         
     def fit(self, df):
         for r in range(len(self.pipe)):
             config = self.pipe[r]
-            for i in self._params.function:
-                if config['type']==i[0]:
-                    if 'lable' in config:
-                        self.pipe[r] = i[1](df[config['variable']], label=df[config['lable']], mode=2, **config['param'])
-                    else:
-                        self.pipe[r] = i[1](df[config['variable']], mode=2, **config['param'])
-                    break
+            if 'lable' in config:
+                self._params.function[config['type']](df[config['variable']], label=df[config['lable']], mode=2, **config['param'])
+            else:
+                self._params.function[config['type']](df[config['variable']], mode=2, **config['param'])
         return self
         
     def transform(self, df, keep_columns=None):
