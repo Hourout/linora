315dab664f3b81ef4c15bc230426c7cc3aaee0b6
JinQing Lee <hourout@163.com>
2023-04-14 16:58:17 +0800

    new version 2.0.0

--- linora/vedio/_vedio_util.py
+++ linora/vedio/_vedio_util.py
@@ -15,13 +15,13 @@ def list_audios(directory, file_format=('mpeg', 'mpg', 'dat', 'mp4', 'avi', 'mov
     return list_images(directory, file_format=file_format)
 
 
-def vedio_to_stream(vedio, batch, data_format='HWCN', dtype='float32'):
+def vedio_to_stream(vedio, batch=1, data_format='HWCN', dtype='float32'):
     """Converts a Vedio instance to a Numpy array.
     
     Args:
         image: Vedio instance.
         batch: each iter batch number.
-        data_format: array data format, eg.'HWCN'.
+        data_format: array data format, eg.'HWCN', 'CHWN'. 'image' return pillow instance.
         dtype: Dtype to use for the returned array.
     Returns:
         A Numpy array iterator.
@@ -29,16 +29,26 @@ def vedio_to_stream(vedio, batch, data_format='HWCN', dtype='float32'):
     container = av.open(vedio._filename)
     container.streams.video[0].thread_type = "AUTO"
     stream = container.streams.video[0]
-    transpose = {'H':0, 'W':1, 'C':2, 'N':3}
     batch_array = []
-    for r, frame in enumerate(container.decode(stream), start=1):
-        batch_array.append(np.expand_dims(frame.to_ndarray(format="rgb24"), axis=-1))
-        if not r%batch:
-            array = np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
-            batch_array = []
-            yield array
-    if batch_array:
-        yield np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
+    
+    if data_format=='image':
+        for r, frame in enumerate(container.decode(stream), start=1):
+            batch_array.append(frame.to_image())
+            if not r%batch:
+                yield batch_array
+                batch_array = []
+        if batch_array:
+            yield batch_array
+    else:
+        transpose = {'H':0, 'W':1, 'C':2, 'N':3}
+        for r, frame in enumerate(container.decode(stream), start=1):
+            batch_array.append(np.expand_dims(frame.to_ndarray(format="rgb24"), axis=-1))
+            if not r%batch:
+                array = np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
+                batch_array = []
+                yield array
+        if batch_array:
+            yield np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
     container.close()
 
     
@@ -47,7 +57,7 @@ def vedio_to_array(vedio, data_format='HWCN', dtype='float32'):
     
     Args:
         image: Vedio instance.
-        data_format: array data format, eg.'HWCN'.
+        data_format: array data format, eg.'HWCN', 'CHWN'.
         dtype: Dtype to use for the returned array.
     Returns:
         A Numpy array.
