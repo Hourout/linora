8c52b5d357458a07e44a8b8563922deba969eb6e
JinQing Lee <hourout@163.com>
2023-04-14 15:47:14 +0800

    new version 2.0.0

--- linora/vedio/_vedio_util.py
+++ linora/vedio/_vedio_util.py
@@ -15,7 +15,17 @@ def list_audios(directory, file_format=('mpeg', 'mpg', 'dat', 'mp4', 'avi', 'mov
     return list_images(directory, file_format=file_format)
 
 
-def vedio_to_stream(vedio, batch, data_format='HWCN'):
+def vedio_to_stream(vedio, batch, data_format='HWCN', dtype='float32'):
+    """Converts a Vedio instance to a Numpy array.
+    
+    Args:
+        image: Vedio instance.
+        batch: each iter batch number.
+        data_format: array data format, eg.'HWCN'.
+        dtype: Dtype to use for the returned array.
+    Returns:
+        A Numpy array iterator.
+    """
     container = av.open(vedio._filename)
     container.streams.video[0].thread_type = "AUTO"
     stream = container.streams.video[0]
@@ -24,20 +34,30 @@ def vedio_to_stream(vedio, batch, data_format='HWCN'):
     for r, frame in enumerate(container.decode(stream), start=1):
         batch_array.append(np.expand_dims(frame.to_ndarray(format="rgb24"), axis=-1))
         if not r%batch:
-            array = np.concatenate(batch_array, axis=-1).transpose(tuple(transpose[i] for i in data_format))
+            array = np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
             batch_array = []
             yield array
     if batch_array:
-        yield np.concatenate(batch_array, axis=-1).transpose(tuple(transpose[i] for i in data_format))
+        yield np.concatenate(batch_array, axis=-1, dtype=dtype).transpose(tuple(transpose[i] for i in data_format))
     container.close()
 
     
-def vedio_to_array(vedio, data_format='HWCN'):
+def vedio_to_array(vedio, data_format='HWCN', dtype='float32'):
+    """Converts a Vedio instance to a Numpy array.
+    
+    Args:
+        image: Vedio instance.
+        data_format: array data format, eg.'HWCN'.
+        dtype: Dtype to use for the returned array.
+    Returns:
+        A Numpy array.
+    """
     container = av.open(vedio._filename)
     container.streams.video[0].thread_type = "AUTO"
     stream = container.streams.video[0]
     transpose = {'H':0, 'W':1, 'C':2, 'N':3}
-    data = np.concatenate([np.expand_dims(frame.to_ndarray(format="rgb24"), axis=-1) for frame in container.decode(stream)], axis=-1)
+    data = np.concatenate([np.expand_dims(frame.to_ndarray(format="rgb24"), axis=-1) for frame in container.decode(stream)],
+                          axis=-1, dtype=dtype)
     if data_format!='HWCN':
         data = data.transpose(tuple(transpose[i] for i in data_format))
     container.close()
