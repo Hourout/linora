6ae4414fd5c26deac2260834020feaa21d3ee563
JinQing Lee <hourout@163.com>
2023-04-27 17:27:21 +0800

    new version 2.0.0

--- linora/vedio/_vedio_io.py
+++ linora/vedio/_vedio_io.py
@@ -48,76 +48,77 @@ def save_vedio(filename, video_array, video_fps, video_codec="libx264", options=
         audio_codec: the name of the audio codec, i.e. "mp3", "aac", etc.
         audio_options: dictionary containing options to be passed into the PyAV audio stream
     """
-    with av.open(filename, mode="w") as container:
-        stream = container.add_stream(video_codec, rate=int(video_fps))
-        stream.pix_fmt = "yuv420p" if video_codec != "libx264rgb" else "rgb24"
-        stream.options = options or {}
-        if isinstance(vedio_array, np.ndarray):
-            assert vedio_array.ndim==4, '`video_array` type error.'
-            stream.width = video_array.shape[1]
-            stream.height = video_array.shape[0]
-            for i in range(vedio_array.shape[-1]):
-                frame = av.VideoFrame.from_ndarray(vedio_array[:,:,:,i], format="rgb24")
-                frame.pict_type = "NONE"
-                for packet in stream.encode(frame):
-                    container.mux(packet)
-        else:
-            if isinstance(vedio_array, list):
-                if isinstance(vedio_array[0], np.ndarray):
-                    assert vedio_array[0].ndim==3, '`video_array` type error.'
-                    stream.width = video_array[0].shape[1]
-                    stream.height = video_array[0].shape[0]
+    container = av.open(filename, mode="w")
+    stream = container.add_stream(video_codec, rate=int(video_fps))
+    stream.pix_fmt = "yuv420p" if video_codec != "libx264rgb" else "rgb24"
+    stream.options = options or {}
+    if isinstance(vedio_array, np.ndarray):
+        assert vedio_array.ndim==4, '`video_array` type error.'
+        stream.width = video_array.shape[1]
+        stream.height = video_array.shape[0]
+        for i in range(vedio_array.shape[-1]):
+            frame = av.VideoFrame.from_ndarray(vedio_array[:,:,:,i], format="rgb24")
+            frame.pict_type = "NONE"
+            for packet in stream.encode(frame):
+                container.mux(packet)
+    else:
+        if isinstance(vedio_array, list):
+            if isinstance(vedio_array[0], np.ndarray):
+                assert vedio_array[0].ndim==3, '`video_array` type error.'
+                stream.width = video_array[0].shape[1]
+                stream.height = video_array[0].shape[0]
+                for i in vedio_array:
+                    frame = av.VideoFrame.from_ndarray(i, format="rgb24")
+                    frame.pict_type = "NONE"
+                    for packet in stream.encode(frame):
+                        container.mux(packet)
+            else:
+                try:
+                    stream.width = video_array[0].width
+                    stream.height = video_array[0].height
                     for i in vedio_array:
-                        frame = av.VideoFrame.from_ndarray(i, format="rgb24")
+                        frame = av.VideoFrame.from_image(i)
                         frame.pict_type = "NONE"
                         for packet in stream.encode(frame):
                             container.mux(packet)
-                else:
-                    try:
-                        stream.width = video_array[0].width
-                        stream.height = video_array[0].height
-                        for i in vedio_array:
-                            frame = av.VideoFrame.from_image(i)
-                            frame.pict_type = "NONE"
-                            for packet in stream.encode(frame):
-                                container.mux(packet)
-                    except:
-                        raise ValueError('`video_array` type error.')
-            else:
-                raise ValueError('`video_array` type error.')
-        for packet in stream.encode():
+                except:
+                    raise ValueError('`video_array` type error.')
+        else:
+            raise ValueError('`video_array` type error.')
+    for packet in stream.encode():
+        container.mux(packet)
+
+    if audio_array is not None:
+        audio_format_dtypes = {
+            "dbl": "<f8",
+            "dblp": "<f8",
+            "flt": "<f4",
+            "fltp": "<f4",
+            "s16": "<i2",
+            "s16p": "<i2",
+            "s32": "<i4",
+            "s32p": "<i4",
+            "u8": "u1",
+            "u8p": "u1",
+        }
+        a_stream = container.add_stream(audio_codec, rate=int(audio_fps))
+        a_stream.options = audio_options or {}
+
+        num_channels = audio_array.shape[0]
+        audio_layout = "stereo" if num_channels > 1 else "mono"
+        audio_sample_fmt = container.streams.audio[0].format.name
+
+        dtype = np.dtype(audio_format_dtypes[audio_sample_fmt])
+
+        frame = av.AudioFrame.from_ndarray(audio_array.astype(dtype), format=audio_sample_fmt, layout=audio_layout)
+        frame.sample_rate = int(audio_fps)
+
+        for packet in a_stream.encode(frame):
             container.mux(packet)
 
-        if audio_array is not None:
-            audio_format_dtypes = {
-                "dbl": "<f8",
-                "dblp": "<f8",
-                "flt": "<f4",
-                "fltp": "<f4",
-                "s16": "<i2",
-                "s16p": "<i2",
-                "s32": "<i4",
-                "s32p": "<i4",
-                "u8": "u1",
-                "u8p": "u1",
-            }
-            a_stream = container.add_stream(audio_codec, rate=int(audio_fps))
-            a_stream.options = audio_options or {}
-
-            num_channels = audio_array.shape[0]
-            audio_layout = "stereo" if num_channels > 1 else "mono"
-            audio_sample_fmt = container.streams.audio[0].format.name
-
-            dtype = np.dtype(audio_format_dtypes[audio_sample_fmt])
-
-            frame = av.AudioFrame.from_ndarray(audio_array.astype(dtype), format=audio_sample_fmt, layout=audio_layout)
-            frame.sample_rate = int(audio_fps)
-
-            for packet in a_stream.encode(frame):
-                container.mux(packet)
-
-            for packet in a_stream.encode():
-                container.mux(packet)
+        for packet in a_stream.encode():
+            container.mux(packet)
+    container.close()
 
         
 def read_vedio(src, start_sec=0, end_sec=np.inf, vedio_format="HWCN", vedio_type=np.uint8, audio_format='CL'):
