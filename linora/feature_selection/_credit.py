e2652f6b50db6ada0af252811bf4c22975cd6d9c
JinQing Lee <hourout@163.com>
2024-02-20 19:23:24 +0800

    new version 2.0.0

--- linora/feature_selection/_credit.py
+++ linora/feature_selection/_credit.py
@@ -24,25 +24,17 @@ def woe(feature, y_true, pos_label=1):
     return t
 
 
-def iv(feature, y_true, pos_label=1):
-    """Calculate series iv value
-    
-    Args:
-        feature: pd.Series, shape (n_samples,), x variable, model feature.
-        y_true: pd.Series, shape (n_samples,), The target variable for supervised learning problems.
-        pos_label: int, default=1, positive label value.
-    
-    Return:
-        series iv value
-    """
+def iv(feature, y_true, max_bins=5, pos_label=1):
     t = pd.DataFrame({'label':y_true, 'feature':feature})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
     label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    def _iv(group, df):
-        a = group.sum()/df.label.sum()
-        b = (group.count()-group.sum())/(t.label.count()-t.label.sum())
-        return (a-b)/np.log(a/b)
-    corr = t.label.sum()/(t.label.count()-t.label.sum())
-    t = t.groupby(['feature']).label.apply(lambda x:_iv(x, t)).sum()
-    return t
+    if t.feature.nunique()>max_bins:
+        t['feature'] = pd.qcut(x=t.feature, q=max_bins, duplicates='drop')
+    df = pd.crosstab(index=t.feature, columns=t.label, margins=False).rename(columns={0:'neg',1:'pos'})
+    df['pos_rate'] = df['pos'].replace({0:1}) / df['pos'].sum()
+    df['neg_rate'] = df['neg'].replace({0:1}) / df['neg'].sum()
+    df['woe'] = np.log(df['pos_rate'] / df['neg_rate'])
+    df['iv'] = (df['pos_rate'] - df['neg_rate']) * df['woe']
+    iv = df['iv'].sum()
+    return iv
