fdb1221e61b663d2c062f5258b157b6a83997799
JinQing Lee <hourout@163.com>
2024-02-20 20:12:42 +0800

    new version 2.0.0

--- linora/feature_selection/_credit.py
+++ linora/feature_selection/_credit.py
@@ -4,12 +4,13 @@ import pandas as pd
 __all__ = ['woe', 'iv']
 
 
-def woe(feature, y_true, pos_label=1):
+def woe(feature, y_true, max_bins=5, pos_label=1):
     """Calculate series woe value
     
     Args:
         feature: pd.Series, shape (n_samples,) x variable, model feature.
         y_true: pd.Series, shape (n_samples,) The target variable for supervised learning problems.
+	max_bins: maximum number of boxes.
         pos_label: int, default=1, positive label value.
     
     Return:
@@ -19,12 +20,27 @@ def woe(feature, y_true, pos_label=1):
     assert t.label.nunique()==2, "`y_true` should be binary classification."
     label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
     t['label'] = t.label.replace(label_dict)
-    corr = t.label.sum()/(t.label.count()-t.label.sum())
-    t = t.groupby(['feature']).label.apply(lambda x:np.log(x.sum()/(x.count()-x.sum())/corr))
-    return t
+    if t.feature.nunique()>max_bins:
+        t['feature'] = pd.qcut(x=t.feature, q=max_bins, duplicates='drop')
+    df = pd.crosstab(index=t.feature, columns=t.label, margins=False).rename(columns={0:'neg',1:'pos'})
+    df['pos_rate'] = df['pos'].replace({0:1}) / df['pos'].sum()
+    df['neg_rate'] = df['neg'].replace({0:1}) / df['neg'].sum()
+    df['woe'] = np.log(df['pos_rate'] / df['neg_rate'])
+    return df
 
 
 def iv(feature, y_true, max_bins=5, pos_label=1):
+    """Calculate series iv value
+    
+    Args:
+        feature: pd.Series, shape (n_samples,) x variable, model feature.
+        y_true: pd.Series, shape (n_samples,) The target variable for supervised learning problems.
+	max_bins: maximum number of boxes.
+        pos_label: int, default=1, positive label value.
+    
+    Return:
+        series iv value
+    """
     t = pd.DataFrame({'label':y_true, 'feature':feature})
     assert t.label.nunique()==2, "`y_true` should be binary classification."
     label_dict = {i:1 if i==pos_label else 0 for i in t.label.unique()}
