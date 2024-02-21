60e8b4b32ffbf507239f3142cfca59a399534c37
JinQing Lee <hourout@163.com>
2024-02-21 14:23:17 +0800

    new version 2.0.0

--- linora/param_search/_HyperParameters.py
+++ linora/param_search/_HyperParameters.py
@@ -109,8 +109,8 @@ class HyperParametersRandom():
                     self.params[name] = int(np.random.randint(config['min_value'], config['max_value']))
                 elif config['mode']=='Choice':
                     self.params[name] = np.random.choice(config['values'], p=config['weight'])
-        		    if 'numpy.int' in str(type(self.params[name])):
-            			self.params[name] = int(self.params[name])
+                    if 'numpy.int' in str(type(self.params[name])):
+                        self.params[name] = int(self.params[name])
                 elif config['mode']=='Dependence':
                     self.params[name] = config['function'](self.params[config['dependent_name']])
         self.params_history[self._update_nums] = self.params.copy()
@@ -296,3 +296,4 @@ class HyperParametersGrid():
                     else:
                         if name in self._rank[rank]:
                             self._rank[rank].remove(name)
+                        
