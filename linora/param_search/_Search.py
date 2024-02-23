bd7c26bf2c587b0854d8bb9a4aa536997f0b8903
JinQing Lee <hourout@163.com>
2024-02-23 19:57:50 +0800

    new version 2.0.0

--- linora/param_search/_Search.py
+++ linora/param_search/_Search.py
@@ -36,7 +36,7 @@ class BaseSearch():
                 import xgboost as xgb
                 assert xgb.__version__>=__xgboost_version__, f'xgboost version should be >={__xgboost_version__}.'
                 self.params.model_init = xgb.XGBClassifier
-            elif model=='LGBMClassifier':
+            elif model=='XGBRegressor':
                 import xgboost as xgb
                 assert xgb.__version__>=__xgboost_version__, f'xgboost version should be >={__xgboost_version__}.'
                 self.params.model_init = xgb.XGBRegressor
@@ -53,22 +53,22 @@ class BaseSearch():
         self.best_params = dict()
         self.best_params_history = dict()
 
-    def search(self, train_data, metrics, valid_data=None,
+    def search(self, train_data, test_data, metrics, valid_data=None,
                iter_num=None, cv=3, metrics_min=True, 
-               speedy=True, speedy_param=(20000, 0.3), 
+               train_test_diff=0.03, train_size=1,
                save_model_dir=None, save_model_name=None):
         """model params search method.
 
         Args:
             train_data: A list of (X, y, sample_weight) tuple pairs to use as train sets.
+            test_data: A list of (X, y, sample_weight) tuple pairs to use as test sets.
             metrics: model metrics function.
             valid_data: A list of (X, y, sample_weight) tuple pairs to use as validation sets.
             iter_num: search count.
             cv: cross validation fold.
             metrics_min: metrics value whether the smaller the better.
-            speedy: whether use speedy method.
-            speedy_param: if use speedy method, test_size will be set, 
-                          test_size = 1-round(min(speedy_param[0], feature.shape[0]*speedy_param[1])/feature.shape[0], 2).
+            train_size: train data size.
+            train_test_diff: train error and test error must be less than `train_test_diff`.
             save_model_dir: str, save model folder, only work with model='XGBClassifier' or 'XGBRegressor'.
             save_model_name: str, save model name prefix, only work with model='XGBClassifier' or 'XGBRegressor'.
         Returns:
@@ -78,16 +78,17 @@ class BaseSearch():
         """
         logger = Logger(name=self.params.name)
         logger.info(f"Start hyperparameter {self.params.method} search.")
+        logger.info(f"train data: {int(len(train_data[0])*train_size)}")
+        logger.info(f"test  data: {int(len(test_data[0]))}")
+        if valid_data is not None:
+            logger.info(f"valid data: {int(len(valid_data[0]))}")
+            cv_score_list = []
         import warnings
         warnings.filterwarnings("ignore")
-        if speedy:
-            test_size = 1-round(min(speedy_param[0], len(train_data[1])*speedy_param[1])/len(train_data[1]), 2)
+        
         if self.params.model_name=='XGBClassifier':
             self._xgb_weight(train_data[1])
         
-        if valid_data is not None:
-            cv_score_list = []
-            
         if self.params.method=='grid':
             if iter_num is None:
                 iter_num = self.hp.cardinality()
@@ -95,29 +96,33 @@ class BaseSearch():
                 iter_num = min(iter_num, self.hp.cardinality())
         if iter_num is None:
             iter_num = 100
+        scoring = None
         for i in range(1, iter_num+1):
             self.hp.update(self.best_params)
             self.params.model = self.params.model_init(**self.hp.params)
             score = []
-            if speedy:
-                for _ in range(cv):
-                    index = train_test_split(train_data[0], train_data[1], test_size, seed=np.random.choice(range(100), 1)[0])
-                    score.append(self._model_fit_predict(train_data, metrics, index, mode=1))
-            else:
-                index_list = kfold(train_data[0], train_data[1], n_splits=cv, seed=np.random.choice(range(100), 1)[0])
-                for n, index in enumerate(index_list):
-                    score.append(self._model_fit_predict(train_data, metrics, index, mode=1))
+            for _ in range(cv):
+                index = train_test_split(train_data[0], train_data[1], 1-train_size, seed=np.random.choice(range(100), 1)[0])
+
+                self._model_fit(train_data, index)
+                train_metrics = self._model_metrics(train_data, metrics)
+                test_metrics = self._model_metrics(test_data, metrics)
+                if (train_metrics-test_metrics)<train_test_diff:
+                    score.append(test_metrics)
+
+            if not score:
+                continue
             cv_score = np.mean(score)
             if valid_data is not None:
                 cv_score_list.append(cv_score)
                 cv_score_list.sort()
                 threshold = cv_score_list[int(len(cv_score_list)*(0.2 if metrics_min else 0.8))]
                 if (metrics_min==True and threshold>=cv_score) or (metrics_min==False and threshold<=cv_score):
-                    cv_score = self._model_fit_predict(valid_data, metrics, index=None, mode=0)
+                    cv_score = self._model_metrics(valid_data, metrics)
                 else:
                     logger.info(f"Model {self.params.method} search progress: {i/iter_num*100:.1f}%, best score: {scoring:.4f}", enter=False if i<iter_num else True)
                     continue
-            if i==1:
+            if scoring is None:
                 scoring = cv_score
             if (metrics_min==True and cv_score<=scoring) or (metrics_min==False and cv_score>=scoring):
                 scoring = cv_score
@@ -134,26 +139,18 @@ class BaseSearch():
         logger.info(f"Model {self.params.method} search best score: {scoring:.4f}", close=True, time_mode=1)
         return self.best_params
 
-    def _model_fit_predict(self, data, metrics, index=None, mode=1):
-        if mode:
-            if len(data)==2:
-                self.params.model.fit(data[0].loc[index[0]], data[1][index[0]])
-            else:
-                self.params.model.fit(data[0].loc[index[0]], data[1][index[0]], sample_weight=data[2][index[0]])
-        if index is None:
-            cv_pred = pd.Series(self.params.model.predict(data[0]), index=data[1].index)
-        else:
-            cv_pred = pd.Series(self.params.model.predict(data[0].loc[index[1]]), index=data[1][index[1]].index)
+    def _model_fit(self, data, index):
         if len(data)==2:
-            if index is None:
-                return metrics(data[1], cv_pred)
-            else:
-                return metrics(data[1][index[1]], cv_pred)
+            self.params.model.fit(data[0].loc[index[0]], data[1][index[0]])
         else:
-            if index is None:
-                return metrics(data[1], cv_pred, sample_weight=data[2])
-            else:
-                return metrics(data[1][index[1]], cv_pred, sample_weight=data[2][index[1]])
+            self.params.model.fit(data[0].loc[index[0]], data[1][index[0]], sample_weight=data[2][index[0]])
+    
+    def _model_metrics(self, data, metrics):
+        pred = pd.Series(self.params.model.predict_proba(data[0])[:,1], index=data[1].index)
+        if len(data)==2:
+            return metrics(data[1], pred)
+        else:
+            return metrics(data[1], pred, sample_weight=data[2])
     
     def _xgb_weight(self, label):
         weight_dict = Counter(label)
